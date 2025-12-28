8254fec17c375aed79f3e52ff6d13fb4699315f1
Dario <dariogaleh@outlook.com>
2025-12-28 17:31:05 +0100

    have readme for github

--- packages/chuka/scripts/build.bash
+++ packages/chuka/scripts/build.bash
@@ -6,7 +6,7 @@ webpack-cli
 
 api-extractor run --local --verbose --config ./config/api-extractor.index.json
 
-cp README.md ./dist/chuka/README.md
+cp ../../README.md ./dist/chuka/README.md
 
 rm -rf temp
 rm -rf dist-ts
