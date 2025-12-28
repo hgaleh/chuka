9eb7479b69fefc43905c5a6272465b8c7f349bfb
Dario <dariogaleh@outlook.com>
2025-12-28 12:40:21 +0100

    remove extra d.ts files

--- scripts/build.bash
+++ scripts/build.bash
@@ -5,13 +5,10 @@ rm -rf dist
 webpack-cli
 
 api-extractor run --local --verbose --config ./config/api-extractor.index.json
-api-extractor run --local --verbose --config ./config/api-extractor.decorators.json
-api-extractor run --local --verbose --config ./config/api-extractor.middlewares.json
-api-extractor run --local --verbose --config ./config/api-extractor.validators.json
 
 cp README.md ./dist/chuka/README.md
 
 rm -rf temp
-rm -rf dist-ts
+# rm -rf dist-ts
 
 node ./scripts/create-package-json.js
